2059caaaf9d6d3098c5773afccc27b83cb2741cb
Learath2 <learath2@gmail.com>
2016-07-28 17:41:03 +0300

    That function doesn't exist anymore.

--- util.h
+++ util.h
@@ -1,3 +1,2 @@
 void *util_dup(const void *src, size_t len);
 int util_tokenize(char *buffer, size_t bufsize, char **tokarr, size_t toksize);
-struct ircmessage util_msg_syntax(char **tokarr, size_t ntok);
